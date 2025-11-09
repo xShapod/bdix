5fe81285ac9ee2675550d08589d06ca623362746
xShapod <sajjadul.me7@gmail.com>
2025-11-10 01:57:14 +0600

    Update build.gradle.kts

--- CtgMovies/build.gradle.kts
+++ CtgMovies/build.gradle.kts
@@ -2,20 +2,10 @@ plugins {
     id("com.android.library")
     kotlin("android")
 }
-
 android {
     namespace = "com.bdix.ctgmovies"
     compileSdk = 34
-
-    defaultConfig {
-        minSdk = 21
-    }
-
-    // TestPlugins expects each module to provide its own manifest
+    defaultConfig { minSdk = 21 }
     sourceSets["main"].manifest.srcFile("src/main/AndroidManifest.xml")
 }
-
-dependencies {
-    implementation(project(":common"))      // provided by TestPlugins
-    implementation("org.jsoup:jsoup:1.16.1")
-}
+dependencies { implementation(project(":common")) }
