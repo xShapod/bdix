3e611d4ffbe286d986880e6b79496efda32be754
xShapod <sajjadul.me7@gmail.com>
2025-11-09 23:10:57 +0600

    Update build.gradle.kts

--- CtgMovies/build.gradle.kts
+++ CtgMovies/build.gradle.kts
@@ -4,17 +4,18 @@ plugins {
 }
 
 android {
-    namespace = "com.bdix.timepassbd"
+    namespace = "com.bdix.ctgmovies"
     compileSdk = 34
 
     defaultConfig {
         minSdk = 21
     }
 
+    // TestPlugins expects each module to provide its own manifest
     sourceSets["main"].manifest.srcFile("src/main/AndroidManifest.xml")
 }
 
 dependencies {
-    implementation(project(":common"))
+    implementation(project(":common"))      // provided by TestPlugins
     implementation("org.jsoup:jsoup:1.16.1")
 }
