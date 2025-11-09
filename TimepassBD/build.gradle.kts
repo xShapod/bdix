68b0189c7be085aeb12ddc502c032f1c71568e22
xShapod <sajjadul.me7@gmail.com>
2025-11-10 02:32:01 +0600

    Update build.gradle.kts

--- TimepassBD/build.gradle.kts
+++ TimepassBD/build.gradle.kts
@@ -6,11 +6,14 @@ plugins {
 android {
     namespace = "com.bdix.timepassbd"
     compileSdk = 34
-    defaultConfig { minSdk = 21 }
+
+    defaultConfig {
+        minSdk = 21
+    }
+
     sourceSets["main"].manifest.srcFile("src/main/AndroidManifest.xml")
 }
 
 dependencies {
-    implementation(project(":common"))
-    implementation("org.jsoup:jsoup:1.16.1")
+    // No :common here either
 }
